c66a393760ddeaa7b18fd71d70d0274474465d68
Sulthon <sultona412@gmail.com>
2023-05-23 14:41:50 +0700

    update: upload image di view create dan edit

--- BlogWeb/Controllers/ArticleController.cs
+++ BlogWeb/Controllers/ArticleController.cs
@@ -1,169 +1,192 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BlogWeb.Models;
 
 namespace BlogWeb.Controllers
 {
-    public class ArticleController : Controller
-    {
-        private readonly BloggingContext _context;
+	public class ArticleController : Controller
+	{
+		private readonly BloggingContext _context;
 
-        public ArticleController(BloggingContext context)
-        {
-            _context = context;
-        }
+        private readonly IWebHostEnvironment _hostingEnvirontment;
 
-        // GET: Article
-        public async Task<IActionResult> Index()
-        {
-            return _context.Articles != null ?
-                        View(await _context.Articles.ToListAsync()) :
-                        Problem("Entity set 'BloggingContext.Articles'  is null.");
-        }
+        public ArticleController(BloggingContext context, IWebHostEnvironment hostingEnvirontment)
+		{
+			_context = context;
+			_hostingEnvirontment = hostingEnvirontment;	
+		}
 
-        // GET: Article/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Articles == null)
-            {
-                return NotFound();
-            }
+		// GET: Article
+		public async Task<IActionResult> Index()
+		{
+			return _context.Articles != null ?
+						View(await _context.Articles.ToListAsync()) :
+						Problem("Entity set 'BloggingContext.Articles'  is null.");
+		}
 
-            var articleModel = await _context.Articles
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (articleModel == null)
-            {
-                return NotFound();
-            }
+		// GET: Article/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null || _context.Articles == null)
+			{
+				return NotFound();
+			}
 
-            return View(articleModel);
-        }
+			var articleModel = await _context.Articles
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (articleModel == null)
+			{
+				return NotFound();
+			}
 
-        // GET: Article/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+			return View(articleModel);
+		}
 
-        // POST: Article/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Description,Excerpt,Author,TimeRead,Category")] ArticleModel articleModel, IFormFile? FeaturedImg)
-        {
-            articleModel.PublishDate = DateTime.Now;
-            if (FeaturedImg != null)
-                articleModel.FeaturedImg = FeaturedImg.FileName;
-            if (ModelState.IsValid)
-            {
-                _context.Add(articleModel);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(articleModel);
-        }
+		// GET: Article/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        // GET: Article/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Articles == null)
-            {
-                return NotFound();
-            }
 
-            var articleModel = await _context.Articles.FindAsync(id);
-            if (articleModel == null)
-            {
-                return NotFound();
-            }
-            return View(articleModel);
-        }
+		// POST: Article/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,Title,Description,Excerpt,Author,TimeRead,Category")] ArticleModel articleModel, IFormFile? FeaturedImg)
+		{
+			articleModel.PublishDate = DateTime.Now;
+			if (FeaturedImg != null)
+			{
+				string nameFile = $"__{DateTime.Now.Day}_{DateTime.Now.Year}_{DateTime.Now.Hour}_{DateTime.Now.Minute}_{articleModel.Author}";
+				string folderPath = Path.Combine(_hostingEnvirontment.WebRootPath, "img");
+				string filePath = Path.Combine(folderPath, nameFile);
+				using(var stream = new FileStream(filePath, FileMode.Create))
+				{
+					await FeaturedImg.CopyToAsync(stream);
+				}
 
-        // POST: Article/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,Excerpt,Author,TimeRead,Category")] ArticleModel articleModel, IFormFile? FeaturedImg)
-        {
-            articleModel.PublishDate = DateTime.Now;
-            if (FeaturedImg != null)
-                articleModel.FeaturedImg = FeaturedImg.FileName;
+				articleModel.FeaturedImg = nameFile;
+			}
+			if (ModelState.IsValid)
+			{
+				_context.Add(articleModel);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(articleModel);
+		}
 
-            if (id != articleModel.Id)
-            {
-                return NotFound();
-            }
+		// GET: Article/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null || _context.Articles == null)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(articleModel);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ArticleModelExists(articleModel.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(articleModel);
-        }
+			var articleModel = await _context.Articles.FindAsync(id);
+			if (articleModel == null)
+			{
+				return NotFound();
+			}
+			return View(articleModel);
+		}
 
-        // GET: Article/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Articles == null)
-            {
-                return NotFound();
-            }
+		// POST: Article/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,Excerpt,Author,TimeRead,Category")] ArticleModel articleModel, IFormFile? FeaturedImg)
+		{
+			articleModel.PublishDate = DateTime.Now;
 
-            var articleModel = await _context.Articles
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (articleModel == null)
-            {
-                return NotFound();
-            }
+			if (FeaturedImg != null)
+			{
+				// NOTE: FITUR KETIKA IMAGE DI UPDATE, IMAGE YANG LAMA DIDELETE
+				bool filePathLama = Path.Exists(_hostingEnvirontment.WebRootPath + "/img/");
 
-            return View(articleModel);
-        }
+				string nameFile = $"__{DateTime.Now.Day}_{DateTime.Now.Year}_{DateTime.Now.Hour}_{DateTime.Now.Minute}_{articleModel.Author}";
+				string folderPath = Path.Combine(_hostingEnvirontment.WebRootPath, "img");
+				string filePath = Path.Combine(folderPath, nameFile);
+				using (var stream = new FileStream(filePath, FileMode.Create))
+				{
+					await FeaturedImg.CopyToAsync(stream);
+				}
 
-        // POST: Article/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Articles == null)
-            {
-                return Problem("Entity set 'BloggingContext.Articles'  is null.");
-            }
-            var articleModel = await _context.Articles.FindAsync(id);
-            if (articleModel != null)
-            {
-                _context.Articles.Remove(articleModel);
-            }
+				articleModel.FeaturedImg = nameFile;
+			}
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+			if (id != articleModel.Id)
+			{
+				return NotFound();
+			}
 
-        private bool ArticleModelExists(int id)
-        {
-            return (_context.Articles?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
-    }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(articleModel);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!ArticleModelExists(articleModel.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(articleModel);
+		}
+
+		// GET: Article/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null || _context.Articles == null)
+			{
+				return NotFound();
+			}
+
+			var articleModel = await _context.Articles
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (articleModel == null)
+			{
+				return NotFound();
+			}
+
+			return View(articleModel);
+		}
+
+		// POST: Article/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			if (_context.Articles == null)
+			{
+				return Problem("Entity set 'BloggingContext.Articles'  is null.");
+			}
+			var articleModel = await _context.Articles.FindAsync(id);
+			if (articleModel != null)
+			{
+				_context.Articles.Remove(articleModel);
+			}
+
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool ArticleModelExists(int id)
+		{
+			return (_context.Articles?.Any(e => e.Id == id)).GetValueOrDefault();
+		}
+	}
 }
