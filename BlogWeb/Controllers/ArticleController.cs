759e0d20e3f03e8cda6658388c763caad6bb1c83
Sulthon <sultona412@gmail.com>
2023-05-24 15:07:00 +0700

    update: ui page home, detail

--- BlogWeb/Controllers/ArticleController.cs
+++ BlogWeb/Controllers/ArticleController.cs
@@ -2,18 +2,19 @@
 using Microsoft.EntityFrameworkCore;
 using BlogWeb.Models;
 
+
 namespace BlogWeb.Controllers
 {
 	public class ArticleController : Controller
 	{
 		private readonly BloggingContext _context;
 
-        private readonly IWebHostEnvironment _hostingEnvirontment;
+		private readonly IWebHostEnvironment _hostingEnvirontment;
 
-        public ArticleController(BloggingContext context, IWebHostEnvironment hostingEnvirontment)
+		public ArticleController(BloggingContext context, IWebHostEnvironment hostingEnvirontment)
 		{
 			_context = context;
-			_hostingEnvirontment = hostingEnvirontment;	
+			_hostingEnvirontment = hostingEnvirontment;
 		}
 
 		// GET: Article
@@ -62,7 +63,7 @@ namespace BlogWeb.Controllers
 				string nameFile = $"__{DateTime.Now.Day}_{DateTime.Now.Year}_{DateTime.Now.Hour}_{DateTime.Now.Minute}_{articleModel.Author}";
 				string folderPath = Path.Combine(_hostingEnvirontment.WebRootPath, "img");
 				string filePath = Path.Combine(folderPath, nameFile);
-				using(var stream = new FileStream(filePath, FileMode.Create))
+				using (var stream = new FileStream(filePath, FileMode.Create))
 				{
 					await FeaturedImg.CopyToAsync(stream);
 				}
@@ -105,8 +106,12 @@ namespace BlogWeb.Controllers
 
 			if (FeaturedImg != null)
 			{
-				// NOTE: FITUR KETIKA IMAGE DI UPDATE, IMAGE YANG LAMA DIDELETE
-				bool filePathLama = Path.Exists(_hostingEnvirontment.WebRootPath + "/img/");
+				string gambarLama = Path.Combine(_hostingEnvirontment.WebRootPath, "img");
+				if (System.IO.File.Exists(gambarLama))
+				{
+					System.IO.File.Delete(gambarLama);
+				}
+
 
 				string nameFile = $"__{DateTime.Now.Day}_{DateTime.Now.Year}_{DateTime.Now.Hour}_{DateTime.Now.Minute}_{articleModel.Author}";
 				string folderPath = Path.Combine(_hostingEnvirontment.WebRootPath, "img");
